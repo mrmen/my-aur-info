77d1c8d8ec0c10c34d884ea5b8999f9d2b93ea80
Thomas Etcheverria <thomas.etcheverria@laposte.net>
2012-03-05 21:24:40 +0100

    add license

--- my-aur-info.sh
+++ my-aur-info.sh
@@ -3,11 +3,31 @@
 # Created by MrMen <tetcheve at gmail dot com>
 # First time Edited  on 02-01-2012 for v-alpha
 #
-# Time-stamp: <2012-02-02 13:43:56 thomas>
+# Time-stamp: <2012-03-05 21:24:03 thomas>
 # 
 # Version : 1-a1
 #
 ###########################################
+#
+#
+# Copyright (C) 2012 - MrMen
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses>.
+#
+#
+##########################################
 #
 # To use this script, you will have to create
 # a new directory named .my-packages in your home.
