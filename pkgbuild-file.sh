20bb9fcad8c32cb69a1e2450c47663f7270fa46c
Thomas Etcheverria <thomas.etcheverria@laposte.net>
2012-03-15 00:23:03 +0100

    pkgbuild-file added

--- pkgbuild-file.sh
+++ pkgbuild-file.sh
@@ -3,19 +3,48 @@
 # this script allow you to know if files used
 # in your pkgbuild have been modified and so the md5sum.
 
-file=$1
-pkgbuild=$2
-TMPDIR=$(mktemp -d)
 
+# file=$1
+# pkgbuild=$2
+package=$1
+
+
+TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
-curl -s -L "${file}" > file
 
-actual=$(md5sum file | cut -d" " -f1)
-old=$(grep [0-9a-e]\{32\} $pkgbuild)
+yaourt -G $package &>/dev/null
+cp $package/PKGBUILD .
+rm -rf $package
 
-if (($actual!=$old)); then
-    echo "\033[1m\033[31m out of date \033[0m"
-fi
+cat PKGBUILD | awk -v parent=0 '{if ($0 ~ "=") {parent=0} ;if (/source/) {parent=1}; if (parent==1) {print}}' | sed -e 's/\(.*=(\)\(.*\)\()\)/\2/g'> sources
+touch md5sum_file
+
+mkdir download
+cd download
+
+for file in `cat ../sources`;do
+    curl -s -L "$file" |> tmp_file
+    md5sum tmp_file | cut -d" " -f1  >> ../md5sum_file
+done
+
+cd ..
+rm -rf download
+
+touch actual
+sed -e "/[0-9a-f]\{32\}/!d; s/\(.*'\)\([0-9a-f]\{32\}\)\('.*\)/\2/g" PKGBUILD >> actual
+
+longueur=`wc -l actual | cut -d" " -f1`
+for i in `seq 1 $longueur`; do
+    old=`sed -n "$i p" actual`
+    current=`sed -n "$i p" md5sum_file`
+    if [ $old != $current ]; then
+	echo `sed -n "$i p" sources`" is out of date"
+    fi
+done
+
+
+cd ..
+rm -r $TMPDIR
 
 exit 0
