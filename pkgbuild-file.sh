d76b9ed0e4be98f60f70117ceb5f4b34dac48dfa
Thomas Etcheverria <thomas.etcheverria@laposte.net>
2012-03-19 20:35:03 +0100

    accept more than one sources and can do the job with " ' or nothing

--- pkgbuild-file.sh
+++ pkgbuild-file.sh
@@ -6,6 +6,12 @@
 
 # file=$1
 # pkgbuild=$2
+
+if (($#!=1)); then
+    exit 0
+fi;
+
+
 package=$1
 
 
@@ -13,18 +19,28 @@ TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
 
+
+
 yaourt -G $package &>/dev/null
 cp $package/PKGBUILD .
 rm -rf $package
 
-cat PKGBUILD | awk -v parent=0 '{if ($0 ~ "=") {parent=0} ;if (/source/) {parent=1}; if (parent==1) {print}}' | sed -e 's/\(.*=(\)\(.*\)\()\)/\2/g'> sources
+
+pkgver=$(sed -e '/pkgver=/!d; s/.*=//g' PKGBUILD)
+pkgname=$(sed -e '/pkgname=/!d; s/.*=//g' PKGBUILD)
+
+cat PKGBUILD | awk -v var=0 '{if ($0 ~ "source") {var=1}; if (var==1) {print}; if (/[\""'"'"'"]?)/) {var=0}}' | sed -e 's/.*=//g' | sed -e "s/\(.*[\"\']\)\(http.*\)\([\"\'].*\)/\2/g" > sources
+sed -i 's/[()]//g' sources
+
+
 touch md5sum_file
 
 mkdir download
 cd download
 
 for file in `cat ../sources`;do
-    curl -s -L "$file" |> tmp_file
+    fichier=$(eval echo $file)
+    wget -O tmp_file $fichier &>/dev/null
     md5sum tmp_file | cut -d" " -f1  >> ../md5sum_file
 done
 
@@ -35,15 +51,21 @@ touch actual
 sed -e "/[0-9a-f]\{32\}/!d; s/\(.*'\)\([0-9a-f]\{32\}\)\('.*\)/\2/g" PKGBUILD >> actual
 
 longueur=`wc -l actual | cut -d" " -f1`
+
+{
+is_out=0
 for i in `seq 1 $longueur`; do
     old=`sed -n "$i p" actual`
     current=`sed -n "$i p" md5sum_file`
-    if [ $old != $current ]; then
-	echo `sed -n "$i p" sources`" is out of date"
+    if [ "$old" != "$current" ]; then
+	is_out=1
+#	eval echo `sed -n "$i p" sources`" is out of date"
     fi
 done
-
-
+if (($is_out)); then 
+    echo -e $package" is out of date : some file are obsolet\n Try to update as soon as posible :)" | mail -s "Package in AUR obsolet" tetcheve@gmail.com
+fi;
+}
 cd ..
 rm -r $TMPDIR
 
